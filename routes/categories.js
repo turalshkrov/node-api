e7e705a1c580034d188dd1fbfbcbf160a43c86cd
tural.shkrov <tural.shkrov@gmail.com>
2024-03-02 00:11:50 +0400

    feat: category router

---
+++ routes/categories.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const Category = require('../models/category');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get('/id', getCategoryById, async (req, res) => {
+  try {
+    res.status(200).json(res.category);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post('/', categoryValidationRules(), categoryValidation, async (req, res) => {
+  try {
+    const { categoryName, description } = req.body;
+    const category = new Category({ categoryName, description });
+    const newCategory = await category.save();
+    res.status(201).json({ 
+      message: 'Category created succesfully', 
+      content: newCategory 
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put('/:id', getCategoryById, categoryValidationRules(), categoryValidation, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { categoryName, description } = req.body;
+    const updatedCategory = await Category.findByIdAndUpdate(id, { categoryName, description });
+    res.status(204).json({
+      message: 'Category updated successfully',
+      content: updatedCategory,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.delete('/:id', getCategoryById, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deletedCategory = await Category.findByIdAndDelete(id);
+    res.status(204).json({
+      message: 'Category deleted successfully',
+      content: deletedCategory,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
