3e7160c262b3b73b2bd4d08b7560b979ff654775
tural.shkrov <tural.shkrov@gmail.com>
2024-03-02 00:02:47 +0400

    feat: category model

---
+++ models/category.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Category = new mongoose.Schema({
+  categoryName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  }
+}, { collection: 'category', versionKey: false });
+
+Category.pre('save', function (next) {
+  this.categoryName = this.categoryName.trim();
+  this.description = this.description.trim();
+  next();
+});
+
+Category.pre('findOneAndUpdate', function (next) {
+  this._update.categoryName = this._update.categoryName.trim();
+  this._update.description = this._update.description.trim();
+  next();
+});
+
+module.exports = mongoose.model('category', Category);
